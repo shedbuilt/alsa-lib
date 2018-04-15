ec322d9fd929871da90364b7fe0deb077a9b37db
Auston Stewart <auston@shedbuilt.net>
2018-04-15 11:53:32 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./configure &&
-make -j $SHED_NUMJOBS &&
-make DESTDIR="$SHED_FAKEROOT" install &&
-install -Dm644 "${SHED_CONTRIBDIR}/asound.conf.sample" "${SHED_FAKEROOT}/etc/asound.conf.sample"
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+install -Dm644 "${SHED_PKG_CONTRIB_DIR}/asound.conf.sample" "${SHED_FAKE_ROOT}/etc/asound.conf.sample"
